ce040939b9932ecf8cd495f49f3af5d327cf0750
Zafer <0970135@hr.nl>
2020-07-24 11:46:33 +0200

    Visual elements built
    The visual elements are built according to the example image I have received. For this, I have used Bootstrap. The use of components is not yet implemented.

---
+++ components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="~/assets/illustration.png" alt="">
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,99 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        helloprint-assignment
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+    <div class="row">
+      <div class="col">
+        <div class="banner">
+          <img src="~/assets/illustration.png" class="banner-img img-fluid">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+
+      <div class="col-md">
+        <div class="game-window">
+          <form>
+            <div class="form-group">
+              <label for="user-input">Enter a number between 0 and 100</label>
+              <input type="text" class="form-control input-sm" id="user-input">
+            </div>
+            <button type="submit" class="btn btn-success btn-block">Submit</button>
+          </form>
+        </div>
+      </div>
+      <div class="col-md">
+        <div class="game-window">
+          <form>
+            <div class="form-group">
+              <label for="user-input">Enter a number between 0 and 100</label>
+              <input type="text" class="form-control input-sm" id="user-input">
+            </div>
+            <button type="submit" class="btn btn-success btn-block">Submit</button>
+          </form>
+        </div>
+      </div>
+      <div class="col-md">
+        <div class="game-window">
+          <form>
+            <div class="form-group">
+              <label for="user-input">Enter a number between 0 and 100</label>
+              <input type="text" class="form-control input-sm" id="user-input">
+            </div>
+            <button type="submit" class="btn btn-success btn-block">Submit</button>
+          </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+  export default {}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .container {
+    padding: 50px;
+    border: 1px solid rgb(170, 165, 165) !important;
+    border-radius: 5px;
+  }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+  .banner {
+    height: 160px;
+    margin-bottom: 20px;
+    overflow: hidden;
+  }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  @media screen and (max-width: 767px) {
+    .banner {
+      height: 100%;
+      margin-bottom: 10%;
+    }
+  }
 
-.links {
-  padding-top: 15px;
-}
+  .banner-img {
+    min-width: 100%;
+  }
+
+  .game-window {
+    color: grey;
+    font-size: 12px;
+    text-align: center;
+    border: 1px solid rgb(202, 199, 199);
+    border-radius: 3px;
+    /* padding: 75px 100px; */
+    padding: 25% 30%;
+    margin-bottom: 20px;
+  }
+
+  .game-window-x {
+    margin: 0 auto;
+    height: 250px;
+    width: 250px;
+    border: 1px solid grey;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+
 </style>
